0d34f650d70d5ded9c690ebd6ec38cf2920d4b7b
Ojaschikara <ojaschikara@email.com>
2024-05-25 13:02:28 +0530

    added

---
+++ react-assignments/a3p1/src/components/StopWatch.jsx
@@ -0,0 +1,36 @@
+// src/components/Stopwatch.jsx
+
+import { useState, useEffect } from "react";
+
+function StopWatch() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((prevCount) => {
+        // If the count reaches 10, clear the interval to stop the timer
+        if (prevCount >= 9) {
+          clearInterval(intervalId);
+        }
+        // Return the updated count
+        return prevCount + 1;
+      });
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    function cleanupFunction() {
+      clearInterval(intervalId);
+    }
+    return cleanupFunction;
+  }, []);
+  
+
+  return (
+    <div style={{ border: "1px dashed red", padding: "10px", margin: "10px" }}>
+      <h1>Stop Watch</h1>
+      <h4>{count}</h4>
+    </div>
+  );
+}
+
+export default StopWatch;
